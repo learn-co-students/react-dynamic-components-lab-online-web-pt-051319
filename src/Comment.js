10a076be2ea2ee57fc18e22d2f4b1790e59d3cd8
Haj B <phajib@gmail.com>
2020-02-04 00:19:53 -0800

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,16 @@
 //your code here
+import React, { Component } from 'react';
+
+export default class Colmment extends Component {
+
+  render() {
+    return (
+      <div className="comment" style={{opacity: null /*replace null with the value*/}}>
+        {/* your conditional code here! */}
+        {this.props.commentText}
+      </div>
+    )
+  }
+
+}
+
